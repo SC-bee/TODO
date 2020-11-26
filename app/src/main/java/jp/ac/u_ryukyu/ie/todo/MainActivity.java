1c9b598e4927db5a1bede6b2bb8bb2513c640c6a
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2020-11-26 11:34:01 +0900

    レイアウトのテスト

--- app/src/main/java/jp/ac/u_ryukyu/ie/todo/MainActivity.java
+++ app/src/main/java/jp/ac/u_ryukyu/ie/todo/MainActivity.java
@@ -1,10 +1,16 @@
 package jp.ac.u_ryukyu.ie.todo;
 
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
 }
