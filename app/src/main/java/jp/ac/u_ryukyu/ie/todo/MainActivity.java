716e71ed62edf78e71ea0bc4d2cd27224082c4f7
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2020-12-03 11:09:44 +0900

    レイアウトのテスト

--- app/src/main/java/jp/ac/u_ryukyu/ie/todo/MainActivity.java
+++ app/src/main/java/jp/ac/u_ryukyu/ie/todo/MainActivity.java
@@ -2,8 +2,11 @@ package jp.ac.u_ryukyu.ie.todo;
 
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckBox checkBox1 = findViewById(R.id.checkBox1);
+
+        Scanner scan = new Scanner(System.in);
+
+        //String text = scan.next();
+
+        //checkBox1.setText(text);
+
+
+
+
     }
 
 }
