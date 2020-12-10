b5cdab160b76a8a25ba856d118fbbc395258a64d
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2020-12-10 11:29:58 +0900

    push 2020/12/10/11:29

--- app/src/main/java/jp/ac/u_ryukyu/ie/todo/MainActivity.java
+++ app/src/main/java/jp/ac/u_ryukyu/ie/todo/MainActivity.java
@@ -2,9 +2,14 @@ package jp.ac.u_ryukyu.ie.todo;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CheckBox checkBox1 = findViewById(R.id.checkBox1);
+        
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ここにボタンを押した時に行いたい処理を書く↓
+                //checkBox1.setText("ボタンが押されました！");
+
+
+            }
+        });
+
 
 
     }
