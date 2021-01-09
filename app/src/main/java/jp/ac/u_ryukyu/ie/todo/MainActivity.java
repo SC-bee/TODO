e92926413cdad6e9f5b3e6ba8f183b180b138551
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-01-10 01:53:48 +0900

    2021/01/10　1:53

--- app/src/main/java/jp/ac/u_ryukyu/ie/todo/MainActivity.java
+++ app/src/main/java/jp/ac/u_ryukyu/ie/todo/MainActivity.java
@@ -15,17 +15,6 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button button;
-    private EditText editText;
-    private CheckBox checkBoxes[] = new CheckBox[10];
-    private float yList[] = new float[10];
-    private int num = 0;
-
-    // 画面のサイズ
-    private int screenWidth;
-    private int screenHeight;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,76 +40,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                table.removeAllViewsInLayout();
+                // DELETEボタンが押されたときの処理
+                int num_row = table.getChildCount(); // tableレイアウト内の行の数を入手
+                for(int i=0; i<num_row; i++){
+                    TableRow tr = (TableRow) table.getChildAt(i);  // tableの子はTableRow
+                    CheckBox cb = (CheckBox) tr.getChildAt(0);  // tableRowの子はCheckBox
+                    if(cb.isChecked()){
+                        table.removeViewAt(i); // checkBoxにcheckが入ってる行を削除
+                        i -= 1;  // 削除したら行がずれるため調整
+                        num_row -= 1;  // 削除したら行の数もずれるため調整
+                    }
+                }
+
 
             }});
-
-
-
-//        button = findViewById(R.id.button);
-//        editText = findViewById(R.id.editText);
-//
-//        checkBoxes[0] = findViewById(R.id.checkBox0);
-//        yList[0] = 200;
-//        checkBoxes[1] = findViewById(R.id.checkBox1);
-//        yList[1] = 300;
-//        checkBoxes[2] = findViewById(R.id.checkBox2);
-//        yList[2] = 400;
-//        checkBoxes[3] = findViewById(R.id.checkBox3);
-//        yList[3] = 500;
-//        checkBoxes[4] = findViewById(R.id.checkBox4);
-//        yList[4] = 600;
-//        checkBoxes[5] = findViewById(R.id.checkBox5);
-//        yList[5] = 700;
-//        checkBoxes[6] = findViewById(R.id.checkBox6);
-//        yList[6] = 800;
-//        checkBoxes[7] = findViewById(R.id.checkBox7);
-//        yList[7] = 900;
-//        checkBoxes[8] = findViewById(R.id.checkBox8);
-//        yList[8] = 1100;
-//        checkBoxes[9] = findViewById(R.id.checkBox9);
-//        yList[9] = 1100;
-//
-//
-//        // ディスプレイのサイズを取得
-//        WindowManager wm = getWindowManager();
-//        Display display = wm.getDefaultDisplay();
-//        Point size = new Point();
-//        display.getSize(size);
-//        screenWidth = size.x;
-//        screenHeight = size.y;
-//
-//
-//        // オブジェクトの初期位置の設定
-//        button.setX(screenWidth - 220f);
-//        button.setY(50f);
-//        editText.setX(80f);
-//        editText.setY(50f);
-//        checkBoxes[0].setY(-100f);
-//        checkBoxes[1].setY(-100f);
-//        checkBoxes[2].setY(-100f);
-//        checkBoxes[3].setY(-100f);
-//        checkBoxes[4].setY(-100f);
-//        checkBoxes[5].setY(-100f);
-//        checkBoxes[6].setY(-100f);
-//        checkBoxes[7].setY(-100f);
-//        checkBoxes[8].setY(-100f);
-//        checkBoxes[9].setY(-100f);
-//
-//
-//
-//        // ここからボタンを押したときの処理
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                checkBoxes[num].setText(editText.getText());
-//                checkBoxes[num].setY(yList[num]);
-//                num += 1;
-//            }
-//        });
-
-
-
 
     }
 
@@ -138,5 +71,9 @@ public class MainActivity extends AppCompatActivity {
         row.addView(chk);
         table.addView(row);
 
+
+
     }
+
+
 }
